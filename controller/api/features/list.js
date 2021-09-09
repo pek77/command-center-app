f95f3a5772d9edd8f62b455b67ae0d549fd9e949
Robin <robin1991526@gmail.com>
2021-09-09 11:10:25 +0800

    Fix

--- controller/api/features/list.js
+++ controller/api/features/list.js
@@ -5,22 +5,25 @@
 const query = require("../mysql.js");
 const SqlString = require('sqlstring');
 var app_az = process.env.app_az || "AZ 1";
+var instantId = process.env.instantId || "instantId";
 
 
 exports.findListByRoleId = function (req, res) {
     if (req.body.id) {
         let id = req.body.id;
         let sql = "SELECT * from content_view where roleId=" + SqlString.escape(id);
+        let obj = {}
         query(sql, function (err, vals) {
             if (err == null) {
                 // console.log(vals);
-                res.send({data:vals,"app_az":app_az})
+                obj["Web-Instant"] = {"instantId":instantId,"AZ":web_az};
+                res.send({data:vals,obj})
             } else {
                 console.log(err);
-                res.send({data:"502","app_az":app_az})
+                res.send({data:"502",obj})
             }
         })
     } else {
-        res.send({data:"403","app_az":app_az})
+        res.send({data:"403",obj})
     }
 };
